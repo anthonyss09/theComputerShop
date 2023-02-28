22c7a96b78368aa7f2f43aa9fcf011c388076acc
anthonyss09 <76715818+anthonyss09@users.noreply.github.com>
2023-02-28 17:39:57 -0500

    changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import connectDb from "./Db/connect.js";
 import authRouter from "./routes/authRouter.js";
 import productRouter from "./routes/productRouter.js";
 import upload from "./utils/fileUpload.js";
+//gile
 
 app.use(express.json());
 
